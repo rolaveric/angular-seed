d0502b7e89ff2d4fc89bc939530c783d8af90488
rolaveric <rolaveric@gmail.com>
2015-03-08 09:50:41 +1100

    Use Babel as the es6 transpiler
    Updated the SystemJS configuration to use Babel instead of Traceur as the es6 transpiler.
    Also changed "index-async.html" to source SystemJS libraries from node_modules instead of bower_components.

--- app/system.config.js
+++ app/system.config.js
@@ -1,10 +1,13 @@
 // Configure module loader
 System.config({
   baseURL: '/app/',
- 
+
   // Set paths for third-party libraries as modules
   paths: {
     'angular': 'bower_components/angular/angular.js',
     'angular-route': 'bower_components/angular-route/angular-route.js'
-  }
+  },
+
+  // opt in to Babel for transpiling over Traceur
+  transpiler: 'babel'
 });
--- bundle.js
+++ bundle.js
@@ -1,14 +1,15 @@
-var builder = require('systemjs-builder'),
+var Builder = require('systemjs-builder'),
  path = require('path');
- 
+
 // load SystemJS config from file
+var builder = new Builder();
 builder.loadConfig('./app/system.config.js')
  .then(function() {
   // Change baseURL to match the file system
   builder.config({ baseURL: path.resolve('./app') });
- 
+
   // Build a self-executing bundle (ie. Has SystemJS built in and auto-imports the 'app' module)
-  return builder.buildSFX('app', 'app/bundle.js', { minify: true, sourceMaps: true});
+  return builder.buildSFX('app', 'app/bundle.js', { minify: false, sourceMaps: true});
  }).catch(function(err) {
   console.error(err);
  });
